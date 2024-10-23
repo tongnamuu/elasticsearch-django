b0b007aeda680d5196cd4247793e25553826216b
tongnamuu <thban33@naver.com>
2024-10-24 02:55:47 +0900

    redirect

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponseRedirect("1")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
