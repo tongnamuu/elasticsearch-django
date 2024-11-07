64635a8a74063822f432397421e68f4fd0d6b7d0
tongnamuu <thban33@naver.com>
2024-11-08 04:18:13 +0900

    sample

--- productSearch/views.py
+++ productSearch/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from elasticsearch import Elasticsearch 
+from elasticsearch import Elasticsearch
+from sentence_transformers import SentenceTransformer 
 
 es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])
+embedding_model = SentenceTransformer('sentence-transformers/all-MiniLM-L12-v2')
 
 def index(request):
     return HttpResponse("main index")
@@ -32,3 +34,8 @@ def get_product(request, _id):
     # If the product is found, return the product details
     product_details = response['hits']['hits'][0]['_source']
     return HttpResponse(f"Product details: {product_details}")
+
+
+                                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                                                    
